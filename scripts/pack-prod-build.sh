e4939dfebe540f1f56f8dcc9a343aa2d64a73954
snehal patil <snehalpatil0904@gmail.com>
2022-10-10 16:58:26 +0530

    Update pack-prod-build.sh

--- scripts/pack-prod-build.sh
+++ scripts/pack-prod-build.sh
@@ -11,7 +11,6 @@ for f  do
     fi
 done 
 ' sh {} +
-cp -r  packages/teacher-app/build/* prod-build/
 cp -r  packages/student-app/build/* prod-build/
 find  prod-build -name  'modules.json' | xargs sed -i 's|http://localhost:[0-9]*||g'
 cd prod-build && tar -cf ../shiksha-ui.tar . && cd ../
