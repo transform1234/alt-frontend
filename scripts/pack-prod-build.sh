558d3feb00805679f10f192a116f2f174883dbf5
snehal patil <snehalpatil0904@gmail.com>
2022-10-10 17:36:56 +0530

    Update pack-prod-build.sh

--- scripts/pack-prod-build.sh
+++ scripts/pack-prod-build.sh
@@ -5,7 +5,7 @@ mkdir prod-build/modules
 find packages \( ! -path packages/common-lib -o ! -path packages/teacher-app \) -type d  -maxdepth 1 -mindepth 1 -exec bash -c '
 for f  do
     # echo $f
-    if [ $f != "packages/common-lib" ] &&  [ $f != "packages/teacher-app" ] ; then
+    if [ $f != "packages/common-lib" ]; then
         echo "Processing ${f//packages\//}"
         cp -rf "$f/build" "prod-build/modules/${f//packages\//}"
     fi
