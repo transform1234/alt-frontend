20aa452fe73566ff1e23ab3d53cd4ad2e894ea7f
Namita g <namita_g@tekditechnologies.com>
2023-04-18 21:54:43 +0530

    Fix : Changes in home text bengali, change iframe code in funactivity

--- packages/student-app/src/pages/FunActivity.js
+++ packages/student-app/src/pages/FunActivity.js
@@ -47,7 +47,8 @@ export default function FunActivity() {
       }}
     >
       {/* <h1>Sample Module</h1> */}
-      <iframe src="https://h5p.org/node/1372413" height={'470px'}/>
+      <iframe src="https://h5p.org/h5p/embed/1372413" width="1019" height="270" frameborder="0" allowfullscreen="allowfullscreen" allow="geolocation *; microphone *; camera *; midi *; encrypted-media *" title="Match the Organisms: The objective of the game is to collect the most pairs. Select card and remember which cards are where and then try selecting its pair..."></iframe>
+      <script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>
     </Layout>
   );
 }
