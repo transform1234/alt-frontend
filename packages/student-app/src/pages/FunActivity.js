70b4d9c9c95cbd6bf55788577d80fbff99beb3b0
Namita g <namita_g@tekditechnologies.com>
2023-04-19 13:01:35 +0530

    remove footerlinks from fun-activity

--- packages/student-app/src/pages/FunActivity.js
+++ packages/student-app/src/pages/FunActivity.js
@@ -19,37 +19,40 @@ export default function FunActivity() {
       _footer={{
         menues: [
           {
-            title: "HOME",
-            icon: "Home4LineIcon",
-            route: "/",
+            title: 'Home',
+            icon: 'Home4LineIcon',
+            moduleName: 'app',
+            route: '/',
+            routeparameters: {}
           },
           {
-            title: "CLASSES",
-            icon: "TeamLineIcon",
-            route: "/classes",
+            title: 'Learn',
+            icon: 'BookLineIcon',
+            moduleName: 'studentprogram',
+            route: '/studentprogram/subjects',
+            routeparameters: {}
           },
           {
-            title: "SCHOOL",
-            icon: "GovernmentLineIcon",
-            route: "/",
+            title: 'Certificate',
+            icon: 'AwardLineIcon',
+            moduleName: 'Certificate',
+            route: '/Certificate',
+            routeparameters: {}
           },
           {
-            title: "MATERIALS",
-            icon: "BookOpenLineIcon",
-            route: "/",
-          },
-          {
-            title: "CAREER",
-            icon: "UserLineIcon",
-            route: "/",
-          },
+            title: 'Settings',
+            icon: 'DashboardLineIcon',
+            moduleName: 'Settings',
+            route: '/Settings',
+            routeparameters: {}
+          }
         ],
       }}
     >
-      {/* <h1>Sample Module</h1> */}
-
+     <>
       <iframe src="https://h5p.org/h5p/embed/1372413" width="1019" height="270" frameborder="0" allowfullscreen="allowfullscreen" allow="geolocation *; microphone *; camera *; midi *; encrypted-media *" title="Match the Organisms: The objective of the game is to collect the most pairs. Select card and remember which cards are where and then try selecting its pair..."></iframe>
       <script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>
-
+      </>
+      </Layout>
   );
 }
