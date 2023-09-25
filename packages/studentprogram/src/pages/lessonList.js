1247e2a458395aaba9a03fe526337a5a105f7e69
Arif <arifmajeed007.as@gmail.com>
2023-09-25 16:12:50 +0530

    Assessment player fix

--- packages/studentprogram/src/pages/lessonList.js
+++ packages/studentprogram/src/pages/lessonList.js
@@ -279,7 +279,7 @@ export default function LessonList({ footerLinks }) {
                   }
                 }}
                 // public_url="http://localhost:5000"
-                public_url="https://alt-shiksha.uniteframework.io/"
+                public_url="https://alt.uniteframework.io"
               />
             </VStack>
           )
