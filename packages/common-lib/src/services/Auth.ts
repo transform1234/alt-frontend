9b40c0b311adc90fb4c3abf4dd1ba75fadc16f48
deepali chavhan <deechavhan098@gmail.com>
2024-09-09 16:13:18 +0530

    fix: changes in auth ts for getting undefine secret key

--- packages/common-lib/src/services/Auth.ts
+++ packages/common-lib/src/services/Auth.ts
@@ -3,14 +3,15 @@ import axios from 'axios'
 export function fetchToken(
   authUrl: string,
   username: string,
-  password: string
+  password: string,
+  secret_key:string
 ): Promise<any> {
   const params = new URLSearchParams()
   params.append('client_id', 'hasura-app')
   params.append('username', username)
   params.append('password', password)
   params.append('grant_type', 'password')
-  params.append('client_secret', `${process.env.REACT_APP_SECRET_KEY}`)
+  params.append('client_secret', secret_key)
 
   const config = {
     headers: {
